8ab7ce3f59bcaed2e1676631970867863b73b1f7
mtnleo <mtnleonardi@gmail.com>
2023-05-06 13:30:39 -0300

    Ball and both gliders working proper collisions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random as rd
 
 WIDTH = 700
 HEIGHT = 400
@@ -22,6 +23,9 @@ def main():
     else:
         cpu_glider = Cpu_Glider(HEIGHT)
 
+    # instantiate ball
+    ball = Ball()
+
     while running:
         clock.tick(60)
 
@@ -30,19 +34,29 @@ def main():
 
             if event.type == pg.QUIT:
                 running = False
-            
-        if keys_pressed[pg.K_DOWN]:
-            player_glider.move_glider_down()
-        if keys_pressed[pg.K_UP]:
-            player_glider.move_glider_up()
 
+        ### 2 players
         if two_players:
             if keys_pressed[pg.K_s]:
                 second_glider.move_glider_down()
             if keys_pressed[pg.K_w]:
                 second_glider.move_glider_up()
 
+        ## main player movement  
+        if keys_pressed[pg.K_DOWN]:
+            player_glider.move_glider_down()
+        if keys_pressed[pg.K_UP]:
+            player_glider.move_glider_up()
+
+
+        ## screen fill & drawing
         screen.fill(DARK_GREY)
+        if two_players:
+            ball.move_ball(glider_1=player_glider, glider_2=second_glider)
+        else:
+            ball.move_ball(glider_1=player_glider, glider_2=cpu_glider)
+        ball.draw_ball(screen)
+
         player_glider.draw_glider(screen)
 
         if not two_players:
@@ -65,25 +79,26 @@ def main():
 class Glider():
 
     rect_width = 6
-    rect_height = 50
+    rect_height = 67
     bottom = HEIGHT
     top = 0
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.rect = pg.Rect(self.x, (self.y - self.rect_height / 2), self.rect_width, self.rect_height)
 
     def draw_glider(self, screen):
-        rect = pg.Rect(self.x, (self.y - self.rect_height / 2), self.rect_width, self.rect_height)
-        pg.draw.rect(screen, WHITE, rect)
+        self.rect = pg.Rect(self.x, (self.y - self.rect_height / 2), self.rect_width, self.rect_height)
+        pg.draw.rect(screen, WHITE, self.rect)
 
     def move_glider_up(self):
-        if (self.y - self.rect_height / 2 != self.top):
-            self.y -= 3
+        if (self.y - self.rect_height / 2 >= self.top):
+            self.y -= 5
 
     def move_glider_down(self):
-        if (self.y + self.rect_height / 2 != self.bottom):
-            self.y += 3
+        if (self.y + self.rect_height / 2 <= self.bottom):
+            self.y += 5
 
 
 class Player_Glider(Glider):
@@ -104,6 +119,48 @@ class Second_Glider(Glider):
     ##########################
     ### BALL CLASSES #########
     ##########################
+
+class Ball():
+    x: int
+    y: int
+    terminal_velocity = 1.5
+
+    def __init__(self):
+        self.radius = 6
+        self.x = WIDTH / 2
+        self.y = HEIGHT / 2
+        self.vector = pg.Vector2(1, -2)
+        self.speed = 2.5
+        self.ball = pg.Rect(self.x - self.radius, self.y - self.radius, self.radius * 2, self.radius * 2)
+
+    def draw_ball(self, screen):
+        self.ball = pg.Rect(self.x - self.radius, self.y - self.radius, self.radius * 2, self.radius * 2)
+        pg.draw.ellipse(screen, WHITE, self.ball)
+
+    def move_ball(self, glider_1, glider_2):
+        if self.detect_collision_y():
+            self.vector.y = self.vector.y * -1
+
+        if self.detect_collision_x(glider_1, glider_2):
+            if self.vector.x <= self.terminal_velocity:
+                self.vector.x = self.vector.x * -1.09
+            else:
+                self.vector.x = self.vector.x * -1.09
+            self.vector.y += rd.uniform(-1.2, 1.2)
+
+        vec_x = self.vector.x * self.speed
+        vec_y = self.vector.y * self.speed
+
+        self.x += vec_x
+        self.y += vec_y
+
+    def detect_collision_y(self):
+        return self.y - self.radius <= 0 or self.y - self.radius >= HEIGHT
+    
+    def detect_collision_x(self, glider_1, glider_2):   
+        return pg.Rect.colliderect(self.ball, glider_1.rect) or pg.Rect.colliderect(self.ball, glider_2.rect)
+
+    
 
 
 if __name__ == "__main__":
